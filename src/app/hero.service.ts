8a961f9f36faa4c042873b7d776b0e67ae1ee7c5
DEEP-LAPTOP\deep <deep9751@gmail.com>
2018-11-08 22:49:24 +0200

    complete services

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,16 +1,17 @@
 import { Injectable } from '@angular/core';
-import { Hero } from './hero'
-import { HEROES } from './mock-heroes'
-import { Observable, of} from 'rxjs'
-
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { Observable, of, from} from 'rxjs';
+import { MessagesService } from './messages.service';
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messagesService: MessagesService ) { }
 
   getHeroes(): Observable<Hero[]> {
+    this.messagesService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
 }
